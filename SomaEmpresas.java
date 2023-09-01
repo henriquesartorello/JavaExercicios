50fce87a02562bb8ddd6ad41e35f0cb46bf89524
Henrique Sartorello Santos <91102389+henriquesartorello@users.noreply.github.com>
2023-09-01 14:24:01 -0300

    Add files via upload

---
+++ SomaEmpresas.java
@@ -0,0 +1,9 @@
+public class SomaEmpresas {
+    public static  void main(String[] args){
+        int janeiro = 15000;
+        int fevereiro = 23000;
+        int marco = 17000;
+        int total = janeiro + fevereiro + marco;
+        System.out.println("O total gasto foi de: " + total);
+    }
+}
